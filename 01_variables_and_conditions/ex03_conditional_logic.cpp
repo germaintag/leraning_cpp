8d18ef42535668304563b2a13636da2f6d249d5e
Germain <187400537+germaintag@users.noreply.github.com>
2026-05-26 02:32:21 +0200

    feat:add conditional logic structures for exercise 3

---
+++ 01_variables_and_conditions/ex03_conditional_logic.cpp
@@ -0,0 +1,39 @@
+/*
+Description: Simple and nested conditional structures in plain language
+*/
+
+#include <iostream>
+using namespace std;
+
+/* ------------------------------------------------------------------------------
+Question 1: Simple Conditional Structure (If ... Then ... Else ... EndIf)
+Task: Write a simple conditional statement based on every day real-life choices.
+It must include a true/false condition and two distinct actions.
+
+   Begin
+       If (I had 10$ ) then
+           I will buy rice and chicken
+       Else
+           I will only buy rice
+       EndIf
+   End
+*/
+
+/* ------------------------------------------------------------------------------
+Question 2: Nested Conditional Structures
+Task: Write two nested conditional statements using the exact model provided.
+
+   Begin
+       If (I had 10$) Then
+          I will buy rice and chicken
+       Else
+           If (I have 25$) Then
+               I will buy a bread
+           Else
+            I will buy a hot dog
+           EndIf
+       EndIf
+   End
+*/
+
+
